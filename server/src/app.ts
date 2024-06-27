79ee43640477883375a35eda6f0bddd3cd568242
bimodwien <bimodwien2@gmail.com>
2024-06-27 15:53:27 +0700

    update: App dan index ts di backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,23 @@
-import express from "express";
+import express, { NextFunction, Request, Response, urlencoded } from "express";
+import cors from "cors";
+import { corsOption } from "./config";
 
 const app = express();
+
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cors(corsOption));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Kanbim API");
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    res.status(500).send({
+      message: "Internal Server Error",
+    });
+  }
+});
 
 export default app;
