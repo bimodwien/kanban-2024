62767014b4c9717c0afbad588c7fa1aa18cf6284
bimodwien <bimodwien2@gmail.com>
2024-08-22 15:50:27 +0700

    feat: halaman utama edit

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,18 @@
+import Finished from '@/components/Finished';
+import InProgress from '@/components/InProgress';
+import Review from '@/components/Review';
+import Todo from '@/components/Todo';
+
 export default function Home() {
   return (
     <>
-      <section className="pt-4 px-10 bg-[#FAF9F6] h-lvh">
-        <div>Nanti semua komponennya akan ditaruh disini</div>
+      <section className="pt-10 px-10 bg-[#FAF9F6] h-lvh">
+        <div className="md:flex md:flex-col md:justify-center md:items-center lg:grid lg:grid-cols-4 lg:gap-5 lg:items-start">
+          <Todo />
+          <InProgress />
+          <Review />
+          <Finished />
+        </div>
       </section>
     </>
   );
---
+++ apps/web/src/components/InProgress.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+type Props = {};
+
+function InProgress({}: Props) {
+  return (
+    <>
+      <section className="border border-[#E5E5E5] bg-[#F9F9F9] p-4 shadow-xl mb-5 rounded-md">
+        <h3 className="font-poppins font-medium mb-5">In Progress</h3>
+        <div className="flex flex-col gap-5">
+          <div className="border border-[#666666] p-3 pb-16 rounded-md bg-white relative">
+            <div className="flex items-center gap-1">
+              <p className="font-poppins font-light text-[#221C1D] bg-[#B8EBB0] px-2 py-1 rounded-full mb-2 inline-block">
+                Low
+              </p>
+              <p className="font-poppins text-base font-semibold text-[#221C1D] py-1 px-2 mb-2">
+                Lorem Ipsum
+              </p>
+            </div>
+            <p className="font-poppins text-base font-medium text-[#221C1D] py-1">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+            <div className="flex gap-3 py-1">
+              <button className="font-poppins text-left px-2 py-1 text-white text-opacity-90 bg-[#DE1D6E] rounded-md">
+                Delete
+              </button>
+              <button className="font-poppins text-left px-2 py-1 text-white text-opacity-90 bg-[#4F1DDE] rounded-md">
+                Edit
+              </button>
+            </div>
+            <p className="font-poppins font-light text-sm pt-2 text-[#666666]">
+              Mar 3, 2020
+            </p>
+            <div className="absolute bottom-0 right-0 mb-2 mr-3 inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-gray-100 rounded-full">
+              <span className="font-medium text-gray-600">A</span>
+            </div>
+          </div>
+
+          <div className="border border-[#666666] p-3 pb-16 rounded-md bg-white relative">
+            <div className="flex items-center gap-1">
+              <p className="font-poppins font-light text-[#221C1D] bg-[#B8EBB0] px-2 py-1 rounded-full mb-2 inline-block">
+                Low
+              </p>
+              <p className="font-poppins text-base font-semibold text-[#221C1D] py-1 px-2 mb-2">
+                Lorem Ipsum
+              </p>
+            </div>
+            <p className="font-poppins text-base font-medium text-[#221C1D] py-1">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
+            <div className="flex gap-3 py-1">
+              <button className="font-poppins text-left px-2 py-1 text-white text-opacity-90 bg-[#DE1D6E] rounded-md">
+                Delete
+              </button>
+              <button className="font-poppins text-left px-2 py-1 text-white text-opacity-90 bg-[#4F1DDE] rounded-md">
+                Edit
+              </button>
+            </div>
+            <p className="font-poppins font-light text-sm pt-2 text-[#666666]">
+              Mar 3, 2020
+            </p>
+            <div className="absolute bottom-0 right-0 mb-2 mr-3 inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-gray-100 rounded-full">
+              <span className="font-medium text-gray-600">A</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default InProgress;
