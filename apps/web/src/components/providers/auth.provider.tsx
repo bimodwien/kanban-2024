78d14378b59b38955bb1d914ffc763b48d177aa8
bimodwien <bimodwien2@gmail.com>
2024-09-09 12:00:54 +0700

    feat: auth provider update for warning linting

--- apps/web/src/components/providers/auth.provider.tsx
+++ apps/web/src/components/providers/auth.provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useAppDispatch } from '@/lib/redux/hooks';
 import { keepLogin } from '@/lib/redux/middleware/auth.middleware';
 
@@ -8,12 +8,15 @@ type Props = { children: React.ReactNode };
 
 const AuthProvider = ({ children }: Props) => {
   const dispatch = useAppDispatch();
-  async function handleKeepLogin() {
+
+  const handleKeepLogin = useCallback(async () => {
     await dispatch(keepLogin());
-  }
+  }, [dispatch]);
+
   useEffect(() => {
     handleKeepLogin();
-  }, []);
+  }, [handleKeepLogin]);
+
   return children;
 };
 
