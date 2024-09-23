ef9fba2dade3038386f51a1147eece5c2df8afef
bimodwien <bimodwien2@gmail.com>
2024-09-23 14:16:51 +0700

    feat: updating button logout just in homepage

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,7 +1,10 @@
+'use client';
 import Link from 'next/link';
 import Logout from './Logout';
+import { usePathname } from 'next/navigation';
 
 export const Header = () => {
+  const pathname = usePathname();
   return (
     <>
       <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
@@ -15,9 +18,11 @@ export const Header = () => {
             </Link>
           </div>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <div className="text-white bg-[#1A73E8] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">
-              <Logout />
-            </div>
+            {pathname === '/' && (
+              <div className="text-white bg-[#1A73E8] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">
+                <Logout />
+              </div>
+            )}
           </div>
         </div>
       </nav>
